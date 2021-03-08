b650f48b21227ed67bb183ee7febe39e79defebe
McLeanRyan <57815498+McLeanRyan@users.noreply.github.com>
2021-03-07 22:41:41 -0500

    Add files via upload

--- GearHoundsTestingCode.java
+++ GearHoundsTestingCode.java
@@ -1,12 +1,14 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+@TeleOp //@TeleOp shows that this code is being run during the 2 minute driver controlled period of the game. You would change this to be @Autonomous if this was in the 30 second autonomous, or preprogrammed period
+public class GearHoundsTestingCode extends OpMode { //extends OpMode tells the program to continuously repeat the code until a driver presses stop. To make the code run through once, change it to say extends LinearOpMode
 
-public class GearHoundsTestingCode extends OpMode {
-    private DcMotor leftFront = null;
+    private DcMotor leftFront = null; //This is where we declare the variables for the motors and servos used on the robot
     private DcMotor leftBack = null;
     private DcMotor rightFront = null;
     private DcMotor rightBack = null;
@@ -17,8 +19,8 @@ public class GearHoundsTestingCode extends OpMode {
     private Servo flipRight = null;
 
     @Override
-    public void init() {
-        leftFront = hardwareMap.dcMotor.get("leftFront");
+    public void init() { //This method is the code that runs in the initialization phase, before the drivers press play
+        leftFront = hardwareMap.dcMotor.get("leftFront"); //These lines of code, in conjunction with the robot configuration done on the drivers station phone tell the robot where to find different hardware devices
         leftBack = hardwareMap.dcMotor.get("leftBack");
         rightFront = hardwareMap.dcMotor.get("rightFront");
         rightBack = hardwareMap.dcMotor.get("rightBack");
@@ -31,16 +33,17 @@ public class GearHoundsTestingCode extends OpMode {
         leftFront.setDirection(DcMotorSimple.Direction.REVERSE);
         leftBack.setDirection(DcMotorSimple.Direction.REVERSE);
 
-        telemetry.addData("flipLeft Pos:", flipLeft.getPosition());
+        telemetry.addData("flipLeft Pos:", flipLeft.getPosition()); //Telemetry is a specialized print function that is used to print messages on to the drivers station phone for the drivers to see
         telemetry.addData("flipRight Pos:", flipRight.getPosition());
 
         telemetry.addLine("Initialization Complete");
-        telemetry.addLine("Press Play to Start");
+        telemetry.addLine("Robot Ready");
+        telemetry.addLine("When you're ready, press play to start driving.");
     }
 
     @Override
-    public void loop() {
-        double px = gamepad1.left_stick_x;
+    public void loop() { //This method contains the code that runs after the drivers have pressed play. This is where you will keep all of your controls.
+        double px = gamepad1.left_stick_x; //This
         if (Math.abs(px) < 0.05) px = 0;
         double py = -gamepad1.left_stick_y;
         if (Math.abs(py) < 0.05) py = 0;
@@ -62,21 +65,21 @@ public class GearHoundsTestingCode extends OpMode {
         leftBack.setPower(plb);
         rightFront.setPower(prf);
         rightBack.setPower(prb);
-        while (gamepad1.right_trigger > 0.2)    {
+        while (gamepad1.right_trigger > 0.2)  { //This loop controls the strafe controls of the robot. Motors can accept a decimal power value of -1 to 1. 1 is full speed forward and -1 is full speed backwards
             leftFront.setPower(1);
             leftBack.setPower(-1);
             rightFront.setPower(-1);
             leftBack.setPower(1);
         }
 
-        while (gamepad1.left_trigger > 0.2)       {
+        while (gamepad1.left_trigger > 0.2)   {
             leftFront.setPower(-1);
             leftBack.setPower(1);
             rightFront.setPower(1);
             leftBack.setPower(-1);
         }
 
-        while (gamepad1.right_bumper)    {
+        while (gamepad1.right_bumper)  { //This loop strafes at half speed
             leftFront.setPower(0.5);
             leftBack.setPower(-0.5);
             rightFront.setPower(-0.5);
@@ -90,22 +93,26 @@ public class GearHoundsTestingCode extends OpMode {
             leftBack.setPower(-0.5);
         }
 
-        while (gamepad2.right_bumper) {
+        if (gamepad2.right_bumper) {
             flywheelLeft.setPower(1);
             flywheelRight.setPower(1);
+        }else {
+            flywheelLeft.setPower(0);
+            flywheelRight.setPower(0);
         }
 
-        while (gamepad2.left_bumper) {
+        if (gamepad2.left_bumper) {
             conveyor.setPower(0.75);
+        } else {
+            conveyor.setPower(0);
         }
 
         /*if (gamepad2.a) {
-
-        }*/
-
+        }
+        */
         if (gamepad2.b) {
-            flipLeft.setPosition(0);
-            flipRight.setPosition(0);
+         flipLeft.setPosition(0); //Servos don't take a power input, but instead you set the position you want them to move to as a decimal value.
+         flipLeft.setPosition(0);
         }
     }
 }
